fadae82d0f3e7aa4612d7534e6145e1da425f3f5
Ruoyi Wang <wang.ruoyi.uw@gmail.com>
2014-05-21 01:21:38 -0400

    initing TCPClient file for A1

---
+++ A2/TCPClient.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys.types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <iffaddrs.h>
+
+int main (int argc, char *argv[]) {
+
+}

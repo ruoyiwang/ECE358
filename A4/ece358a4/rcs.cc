c5936235085c56a3040c9f53498b95bebad0d169
Roy <wang.ruoyi.uw@gmail.com>
2014-07-14 03:58:41 -0500

    adding a bunch of code to rcs connection stuffs

--- A4/ece358a4/rcs.cc
+++ A4/ece358a4/rcs.cc
@@ -1,5 +1,27 @@
 #include "ucp.h"
 #include "rcs.h"
+#include <map>
+
+using namespace std;
+
+int ack_for_server;
+map<int, int> listening_sockets;
+
+void initPacket(packet* p) {
+    p->ack_num = -1;
+    p->seq_num = -1;
+    p->checksum = 0;
+    p->buff_len = 0;
+    p->type = 0;
+    p->flags = 0;
+
+    return;
+}
+
+int getSynNum(int sockfd) {
+    static int acknum = 0;
+    return (acknum++) % SYN_NUM_MAX;
+}
 
 int rcsSocket() {
     return ucpSocket();
@@ -14,42 +36,100 @@ int rcsGetSockName(int sockfd; struct sockaddr_in * addr) {
 }
 
 int rcsListen(int sockfd) {
-    // udp no listen, what do
+    // check the flags and stuff
+    if (listening_sockets.find(sockfd) == listening_sockets.end()) {
+        // if never listened before
+        listening_sockets[sockfd] = 1;
+        return 0;
+    }
+    else if (listening_sockets.find(sockfd)->second != 1) {
+        // if we ever somehow "unlistened", we can listen again
+        listening_sockets[sockfd] = 1;
+        return 0;
+    }
 
-    // user get sock name to get the addr
-
-    return 0;
+    // else the port is listening... actually
+    // TODO: set up status flag
+    return -1;
 }
 
 int rcsAccept(int sockfd, struct sockaddr_in ∗ addr) {
-    handshake hs1;
-    // receive shits
+    packet p2;
+    int rcv_result;
+    initPacket(&p2);
+    int y = getSynNum(sockfd);
+    while (true) {
+        // receive shits
+        ucpRecvFrom(sockfd, &p2, sizeof(packet), addr);
 
-    // choose init seq num, y
+        if (!(p2.flags & SYN_BIT_MASK)) {
+            // if the syn bit is not set, start over
+            continue;
+        }
 
-    // send TCP SYNACK msg, acking SYN
+        // choose init seq num, y
+        p2.flags = p2.flags | ACK_BIT_MASK;
+        p2.ack_num = p2.seq_num + 1;
+        p2.seq_num = y;
+        // send TCP SYNACK msg, acking SYN
+        ucpSendTo(sockfd, &p2, sizeof(packet), addr);
 
+        // received ACK(y)
+        initPacket(&p2);
+        ucpRecvFrom(sockfd, &p2, sizeof(packet), addr);
+        if (!(p2.flags & ACK_BIT_MASK) || p2.ack_num != y+1) {
+            // if the ack bit is not set or wrong ack num start over
+            continue;
+        }
+        else {
+            // indicates client is live
+            
+            // make socket, bind it, and return it
+            int new_socketfd = rcsSocket();
+            rcsBind(new_socketfd, addr);
+            return new_socketfd;
+        }
+    }
 
-    // received ACK(y)
-
-    // indicates client is live
+    // if it gets here somehow, then idk =___=
+    return -1;
 }
 
 int rcsConnect(int sockfd, struct sockaddr_in ∗ addr) {
-    handshake hs1;
-    // choose init seq num, x
-    hs1.seq = 11;
+    ucpSetSockRecvTimeout(sockfd, TIME_OUT);
+    sockaddr recv_addr;
+    packet p1;
+    int x = getSynNum(sockfd);
+    int tries = 5;
+    while(tries--) {
+        initPacket(&p1);
+        // choose init seq num, x
+        p1.seq = x;
 
-    // send TCP SYN msg
-    hs1.syn_bit = 1;
-    ucpSendTo(sockfd, &hs1, sizeof(hs1), addr);
+        // send TCP SYN msg
+        p1.flags = p1.flags | SYN_BIT_MASK;
+        ucpSendTo(sockfd, &p1, sizeof(packet), addr);
+        
+        // received SYNACK(x)
+        initPacket(&p1);
+        if (ucpRecvFrom(sockfd, &p1, sizeof(packet), &recv_addr) == -1) {
+            continue;
+        }
+        // indicates server is live;
 
 
-    // received SYNACK(x)
+        // send ACK for SYNACK
+        ack_for_server = p1.seq_num + 1;
+        initPacket(&p1);
+        p1.flags = p1.flags | ACK_BIT_MASK;
+        p1.ack_num = ack_for_server;
+        if (ucpRecvFrom(sockfd, &p1, sizeof(packet), &recv_addr) == -1) {
+            return 0;
+        }
+    }
 
-    // indicates server is live;
-
-    // send ACK for SYNACK;
-
-    // this segment may contain client-to-server data
+    // "timed out"
+    // TODO: set some err flag or some shit
+    return -1;
 }
+
--- A4/ece358a4/rcs.h
+++ A4/ece358a4/rcs.h
@@ -3,19 +3,27 @@
 
 #define BUFFER_SIZE     128     // Bytes?
 #define TIME_OUT        100    // ms, yeah idk how long is good lol
+#define SYN_NUM_MAX     10000
+#define WINDOW_SIZE     5
+#define SYN_BIT_MASK    1
+#define ACK_BIT_MASK    2
 
 typedef struct packet_t {
     int ack_num;
+    int seq_num;
     int checksum;
-    void * buffer;
+    char[BUFFER_SIZE] buff;
+    int buff_len;
+    int type;
+    char flags;
 } packet;
 
-typedef struct handshake_t {
-    int syn_bit;
-    int seq;
-    int ack_bit;
-    int ack_num;
-} handshake;
+// typedef struct handshake_t {
+//     int syn_bit;
+//     int seq;
+//     int ack_bit;
+//     int ack_num;
+// } handshake;
 
 int rcsSocket();
 int rcsBind(int sockfd; struct sockaddr_in * addr);
@@ -24,6 +32,5 @@ int rcsListen(int sockfd);
 
 int rcsAccept(int sockfd, struct sockaddr_in ∗ addr);
 int rcsConnect(int sockfd, struct sockaddr_in ∗ addr);
-
 
 #endif
